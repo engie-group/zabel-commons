f011fd347cf13f76d2f61cf05f064f87c5e81191
Martin Lafaix <martin.lafaix@external.engie.com>
2020-02-25 19:09:43 +0100

    Fixing packages detection

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 
     license=' Eclipse Public License 2.0',
 
-    packages=find_namespace_packages(include=['zabel.commons.*']),
+    packages=find_namespace_packages(include=['zabel.*']),
 
     install_requires=['requests>=2.23'],
     python_requires='>= 3.6.5',
