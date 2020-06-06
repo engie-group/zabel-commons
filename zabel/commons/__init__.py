3cc45fcb12f7a17bd633c6d6fd677246d2b9e1a9
Martin Lafaix <martin.lafaix@external.engie.com>
2020-06-06 15:11:43 +0200

    Updating README, bumping to 1.0.1

--- zabel/commons/__init__.py
+++ zabel/commons/__init__.py
@@ -7,13 +7,12 @@
 # SPDX-License-Identifier: EPL-2.0
 
 """
-The transverse **zabel.commons** package
+The transverse **zabel.commons** package.
 
 The **zabel.commons** package collects transverse classes, exceptions,
 and helper functions.
 
-It provides six modules, #::zabel.commons.credentials,
-#::zabel.commons.exceptions, #::zabel.comons.interfaces,
-#::zabel.commons.sessions, #::zabel.commons.servers, and
-#::zabel.commons.utils.
+It provides five modules, #::zabel.commons.exceptions,
+#::zabel.comons.interfaces, #::zabel.commons.sessions,
+#::zabel.commons.servers, and #::zabel.commons.utils.
 """
