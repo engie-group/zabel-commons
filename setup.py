98ae9c9cfabf41c3473f65dcaa3f5d0649b45370
Martin Lafaix <mlafaix@henix.com>
2023-03-27 14:16:25 +0200

    Bumping to 1.6.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='zabel-commons',
-    version='1.5.1',
+    version='1.6.0',
     description='The Zabel transverse **commons** library',
     long_description=long_description,
     long_description_content_type='text/markdown',
